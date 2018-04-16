fd3d3e23c56f2ccbfa09753797ff623b1d7c17c2
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-16 15:48:32 +0900

    + Trailing Closure

--- AlertController/ViewController.swift
+++ AlertController/ViewController.swift
@@ -19,10 +19,15 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: Any) {
         let myAlert = UIAlertController(title: "알림", message: "설정된 시간이 되었습니다.!!", preferredStyle: UIAlertControllerStyle.alert)
         
-        let okAction = UIAlertAction(title: "종료", style: .default, handler: {(ACTION: UIAlertAction) -> Void in
+        
+        let okAction = UIAlertAction(title: "종료", style: .default) {(ACTION: UIAlertAction) -> Void in
             self.view.backgroundColor = UIColor.red
             
-            })
+            }
+        
+        //세번째 인자를 클로져 사용
+        //후행 클로져(Trailing Closure) 일때 핸들러 지우고 그앞에 ()
+        
         
         let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: {(ACTION: UIAlertAction) -> Void in
             self.view.backgroundColor = UIColor.green })
